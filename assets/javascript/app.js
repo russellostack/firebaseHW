b5298f5fe51a091885200b4b53e20b73116700b2
Russell Stack <russellostack@gmail.com>
2019-03-14 11:59:35 -0400

    added moment.js function checker, add table row function, and an arrival time calculator

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,6 +9,8 @@ var config = {
   firebase.initializeApp(config);
 
   var dataRef = firebase.database();
+
+
 $("#submit").on("click",function(event){
     event.preventDefault();
     trainName = $("#trainName-input").val().trim();
@@ -18,3 +20,47 @@ $("#submit").on("click",function(event){
 
 
 })
+
+function addRow(){
+  var newRow = $("<tr>").append(
+    $("<td>").text(train_name),
+    $("<td>").text(destination),
+    $("<td>").text(frequency),
+    $("<td>").text(next_arrival),
+    $("<td>").text(minutes_away),
+  );
+  $("#t > tbody").append(newRow);
+};
+
+database.ref().on("child_added", function(document){
+
+  train_name = document.val().TrainName;
+  destination = document.val().Destination;
+  frequency = document.val().Frequency;
+  next_arrival = document.val().NextArrival;
+  minutes_away = document.val().MinutesAway;
+  addRow();
+});
+
+
+function timeDisplay(){
+  var date = new Date();
+  var hours = date.getHours() <10 ? "0" + date.getHours() : date.getHours();
+  var minutes = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
+  time = hours + ":" + minutes;
+  return time;
+};
+function validTime(time){
+  return (parseInt(time[0]) != NaN && parseInt(time[1])  != NaN && parseInt(time[3])  != NaN && parseInt(time[4]) != NaN);
+}
+
+function calcArrival(){
+  var trainTime = moment.(first_train_time,"hh:mm").subtract(1,"years");
+  var minuteDifference = moment().diff(moment(trainTime), "minutes");
+  var remainder = minuteDifference %frequency;
+
+  minutes_away = frequency - remainder;
+  var nextTrain = moment().add(minutes_away, "minutes");
+  next_arrival = moment.(nextTrain).format("hh:mm");
+  
+}
