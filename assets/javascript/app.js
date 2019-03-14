3e56e4f0f7c2f6d88d1ae4bb0b093b41e06a324d
russellostack <32462495+russellostack@users.noreply.github.com>
2019-03-14 15:16:12 -0400

    some js added, cleaning up stuff i dont understand

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,66 +1,73 @@
+
+
+
+
 var config = {
     apiKey: "AIzaSyCs3K5zwuOuS0odq89IpPLC7HnXTOcDqgI",
     authDomain: "recent-user-with-all-use-e8e76.firebaseapp.com",
     databaseURL: "https://recent-user-with-all-use-e8e76.firebaseio.com",
     projectId: "recent-user-with-all-use-e8e76",
     storageBucket: ""
-  };
+};
 
-  firebase.initializeApp(config);
+firebase.initializeApp(config);
+var dataRef = firebase.database();
 
-  var dataRef = firebase.database();
 
-
-$("#submit").on("click",function(event){
+$("#submit").on("click", function (event) {
+    var trainName = "";
+    var destination = "";
+    var frequency = "";
+    var firstTrainTime = "";
     event.preventDefault();
-    trainName = $("#trainName-input").val().trim();
-    destination = $("#destination-input").val().trim();
-    frequency = $("#frequency-input").val().trim();
-    firstTrainTime = $("#firstTrainTime-input").val().trim();
+    trainName = $("#trainNameInput").val().trim();
+    destination = $("#destinationInput").val().trim();
+    frequency = $("#frequencyInput").val().trim();
+    firstTrainTime = $("#firstTrainTimeInput").val().trim();
 
 
 })
 
-function addRow(){
-  var newRow = $("<tr>").append(
-    $("<td>").text(train_name),
-    $("<td>").text(destination),
-    $("<td>").text(frequency),
-    $("<td>").text(next_arrival),
-    $("<td>").text(minutes_away),
-  );
-  $("#t > tbody").append(newRow);
+function addRow() {
+    var newRow = $("<tr>").append(
+        $("<td>").text(train_name),
+        $("<td>").text(destination),
+        $("<td>").text(frequency),
+        $("<td>").text(next_arrival),
+        $("<td>").text(minutes_away),
+    );
+    $("#trainTable > tbody").append(newRow);
 };
 
-database.ref().on("child_added", function(document){
+database.ref().on("child_added", function (document) {
 
-  train_name = document.val().TrainName;
-  destination = document.val().Destination;
-  frequency = document.val().Frequency;
-  next_arrival = document.val().NextArrival;
-  minutes_away = document.val().MinutesAway;
-  addRow();
+    train_name = document.val().TrainName;
+    destination = document.val().Destination;
+    frequency = document.val().Frequency;
+    next_arrival = document.val().NextArrival;
+    minutes_away = document.val().MinutesAway;
+    addRow();
 });
 
 
-function timeDisplay(){
-  var date = new Date();
-  var hours = date.getHours() <10 ? "0" + date.getHours() : date.getHours();
-  var minutes = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
-  time = hours + ":" + minutes;
-  return time;
+function timeDisplay() {
+    var date = new Date();
+    var hours = date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
+    var minutes = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
+    time = hours + ":" + minutes;
+    return time;
 };
-function validTime(time){
-  return (parseInt(time[0]) != NaN && parseInt(time[1])  != NaN && parseInt(time[3])  != NaN && parseInt(time[4]) != NaN);
+function validTime(time) {
+    return (parseInt(time[0]) != NaN && parseInt(time[1]) != NaN && parseInt(time[3]) != NaN && parseInt(time[4]) != NaN);
 }
 
-function calcArrival(){
-  var trainTime = moment.(first_train_time,"hh:mm").subtract(1,"years");
-  var minuteDifference = moment().diff(moment(trainTime), "minutes");
-  var remainder = minuteDifference %frequency;
+function calcArrival() {
+    var trainTime = moment.(first_train_time, "hh:mm").subtract(1, "years");
+    var minuteDifference = moment().diff(moment(trainTime), "minutes");
+    var remainder = minuteDifference % frequency;
 
-  minutes_away = frequency - remainder;
-  var nextTrain = moment().add(minutes_away, "minutes");
-  next_arrival = moment.(nextTrain).format("hh:mm");
-  
+    minutes_away = frequency - remainder;
+    var nextTrain = moment().add(minutes_away, "minutes");
+    next_arrival = moment.(nextTrain).format("hh:mm");
+
 }
